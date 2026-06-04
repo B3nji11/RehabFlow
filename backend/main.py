b8304edcaccb2d42434a367e50e2c5595fb07d5b
Kai Jin <siahkaijin@gmail.com>
2026-06-04 11:36:47 +0800

    Minor changes before backend

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,24 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+import os
+import supabase
+from dotenv import load_dotenv
 
+
+# temporary import the xgboost model as a placeholder
+import xgboost
+MODEL = xgboost.XGBClassifier()
+MODEL.load_model("C:/Projects/RehabFlow/ml/MODELS/squats_temp.json")
+
+load_dotenv()
 app = FastAPI()
+db : supabase.Client = supabase.create_client(
+    os.environ.get("SUPABASE_URL"),
+    os.environ.get("SUPABASE_KEY")
+)
 
 @app.get("/")
-async def root():
-    return {"message" : "This is a test"}
+def root():
+    return {"test":"this is a test"}
+
+
